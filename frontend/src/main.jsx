97f743ace0ead15ce8b910d208038f4bb7cabd2d
jEmeralds <kenyanjohnson.254@gmail.com>
2025-12-06 17:34:02 +0400

    Restore ThemeProvider to fix blank screen

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import { AuthProvider } from './context/authContext'
+import { ThemeProvider } from './context/ThemeContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
