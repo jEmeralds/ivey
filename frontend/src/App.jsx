52c6d9e0995e5c63f69c4f13e842742408a1a3c1
jEmeralds <kenyanjohnson.254@gmail.com>
2026-03-08 23:24:48 +0300

    fix: render ChatWidget in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -38,6 +38,7 @@ function App() {
               <Route path="*" element={<Home />} />
             </Routes>
           </main>
+          <ChatWidget />
           <Footer />
         </div>
       </ThemeProvider>
